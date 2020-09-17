d780335a780cf2ae99faf62a59db606ac2df77f6
Kasey Jones <joneskm72@gmail.com>
2020-09-17 14:57:01 -0400

    Updated constructors

--- db/db.js
+++ db/db.js
@@ -8,7 +8,38 @@ class db {
   findAllDepartments() {
     return this.connection.query("SELECT * FROM department")
   }
-  findAllEmployees()
+  findAllEmployees() {
+    return this.connection.query("SELECT * FROM employee")
+  }
   
-  findAllRoles()
+  findAllRoles() {
+    return this.connection.query("SELECT * FROM employee_role")
+  }
+
+  createDeptartment(dept_name) {
+    return this.connection.query("INSERT INTO department SET ?", {
+      dept_name: dept_name
+    });
+  }
+  createEmployeeRole(role_title, role_salary, department_id) {
+    return this.connection.query("INSERT INTO employee_role SET ?", {
+      role_title: role_title,
+      role_salary: role_salary,
+      department_id: department_id
+    });
+  }
+
+  createEmployee(first_name, last_name, role_id) {
+    return this.connection.query("INSERT INTO employee SET ?", {
+      first_name: first_name,
+      last_name: last_name,
+      role_id: role_id
+    });
+  }
+
+  employeesByDepartment() {
+    return this.connection.query("SELECT employee.first_name, employee.last_name, employee.role_id FROM employee")
+  }
 }
+
+module.exports = new DB(connection);
